d9bd080c4aeb6111aa4ff9428838d46e9c3dce06
jaceituno16 <javiacei@ucm.es>
2024-10-10 10:33:52 +0200

    Falta codigo de las conexiones

--- src/integracion/transacciones/TransactionFactory.java
+++ src/integracion/transacciones/TransactionFactory.java
@@ -1,51 +1,19 @@
-/**
- * 
- */
+
 package integracion.transacciones;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author Usuario
- * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
-public class TransactionFactory {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+public abstract class TransactionFactory {
+	
 	private static TransactionFactory transactionFactory;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	private TransactionManagerImp transactionManagerImp;
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public static TransactionFactory getInstance() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+	public synchronized static TransactionFactory getInstance() {
+		
+		if (transactionFactory == null){
+			transactionFactory = new TransactionFactoryImp();
+		}
+		
+		return transactionFactory;
+		
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public Transaction nuevaTransaccion() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
+	public abstract Transaction nuevaTransaccion();
 }
