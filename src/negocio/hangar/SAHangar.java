ddea9030d4947cbbf841f57658ea5d0e20cbcc02
paulal20@ucm.es <PAULA LOPEZ SOLLA>
2024-10-18 08:13:01 +0200

    anotaciones

--- src/negocio/hangar/SAHangar.java
+++ src/negocio/hangar/SAHangar.java
@@ -6,7 +6,8 @@ public interface SAHangar {
 
 	public int altaHangar(THangar tHangar);
 
-	public boolean bajaHangar(int id);
+	public boolean bajaHangar(int id);//LLAMAR A FUNCION DE COMPROBAR Q NO TENGA AVIONES ACTIVOS PARA PODER DAR DE BAJA
+	//NO SE PUEDE HACER HASTA Q NO ESTÉ HECHO AVION :)
 
 	public THangar consultarHangarPorId(int id);
 
