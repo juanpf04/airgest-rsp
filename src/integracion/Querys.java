d0099eca46c12798b9ac70986d889fb5a3385583
arturito-7 <146654122+arturito-7@users.noreply.github.com>
2024-10-21 09:58:29 +0200

    activo es boolean y añadido for update a querys

--- src/integracion/Querys.java
+++ src/integracion/Querys.java
@@ -11,15 +11,15 @@ public class Querys {
 	//HANGAR
 	public static String alta_hangar = "INSERT INTO HANGAR (Stock, direccion , espacio_almacenaje, coste_dia, activo) VALUES (?, ?, ?, ?, true)";
 	public static String baja_hangar = "UPDATE HANGAR SET activo = false WHERE id = ?";
-	public static String leerHangarPorId = "SELECT * FROM HANGAR WHERE id = ?";
-	public static String consultarTodosHangares = "SELECT * FROM HANGAR";
+	public static String leerHangarPorId = "SELECT * FROM HANGAR WHERE id = ? FOR UPDATE";
+	public static String consultarTodosHangares = "SELECT * FROM HANGAR FOR UPDATE";
 	public static String modificarHangar = "UPDATE HANGAR SET Stock = ?, direccion = ?, espacio_almacenaje = ?, coste_dia = ?, activo = ? WHERE id = ?";
-	public static String leerHangarPorDireccion = "SELECT * FROM HANGAR WHERE direccion = ?";
+	public static String leerHangarPorDireccion = "SELECT * FROM HANGAR WHERE direccion = ? FOR UPDATE";
 	public static String actualizaStock = "UPDATE HANGAR SET Stock = ? WHERE id = ?";
 	
 	public static String vincularPersonalHangar = "INSERT INTO PERSONAL_HANGAR (Id_personal, Id_hangar) VALUES (?, ?)";
 	public static String desvincularPersonalHangar = "DELETE FROM PERSONAL_HANGAR WHERE Id_personal = ?, Id_hangar = ?";
-	public static String comprobarvinculacionPersonalHangar = "SELECT COUNT(*) AS NUM FROM PERSONAL_HANGAR WHERE Id_personal = ?, Id_hangar = ?";
+	public static String comprobarvinculacionPersonalHangar = "SELECT COUNT(*) AS NUM FROM PERSONAL_HANGAR WHERE Id_personal = ?, Id_hangar = ? FOR UPDATE";
 
 
 	
