b1970b7e069cf9f6e62daf54f1b746ac2984b4c2
acatocaton <alejarel@ucm.es>
2024-11-21 10:22:52 +0100

    AltaMarca

--- src/presentacion/controlador/comandos/marca/AltaMarca.java
+++ src/presentacion/controlador/comandos/marca/AltaMarca.java
@@ -1,27 +1,25 @@
-/**
- * 
- */
+
 package presentacion.controlador.comandos.marca;
 
 import presentacion.controlador.comandos.Comando;
+import negocio.marca.SAMarca;
+import negocio.marca.TMarca;
+import negocio.factoria.FactoriaNegocioMall;
 import presentacion.controlador.Contexto;
+import presentacion.controlador.Evento;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author javia
-* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-*/
 public class AltaMarca implements Comando {
-	/** 
-	* (non-Javadoc)
-	* @see Comando#ejecutar(Object datos)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	@Override
 	public Contexto ejecutar(Object datos) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		FactoriaNegocioMall fn = FactoriaNegocioMall.getInstance();
+		SAMarca sa = fn.crearSAMarca();
+		int id = sa.altaMarca((TMarca) datos);
+		Evento evento = null;
+		if (id != -1)
+			evento = Evento.VISTA_EXITO_ALTA_MARCA;
+		else
+			evento = Evento.VISTA_FALLO_ALTA_MARCA;
+
+		return new Contexto(evento, id);
 	}
 }
