2892349e39c528e6fcb69a57332a242726a778cc
acatocaton <alejarel@ucm.es>
2024-11-22 18:40:41 +0100

    Comando ModificarProveedor

--- src/presentacion/controlador/comandos/producto/ModificarProducto.java
+++ src/presentacion/controlador/comandos/producto/ModificarProducto.java
@@ -1,27 +1,25 @@
-/**
- * 
- */
 package presentacion.controlador.comandos.producto;
 
 import presentacion.controlador.comandos.Comando;
+import negocio.factoria.FactoriaNegocioMall;
+import negocio.producto.SAProducto;
+import negocio.producto.TProducto;
 import presentacion.controlador.Contexto;
+import presentacion.controlador.Evento;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author javia
-* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-*/
 public class ModificarProducto implements Comando {
-	/** 
-	* (non-Javadoc)
-	* @see Comando#ejecutar(Object datos)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	@Override
 	public Contexto ejecutar(Object datos) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		FactoriaNegocioMall fn = FactoriaNegocioMall.getInstance();
+		SAProducto sp = fn.crearSAProducto();
+		boolean exito = sp.modificarProducto((TProducto) datos);
+		Evento evento = null;
+		if (exito) {
+			evento = Evento.VISTA_EXITO_MODIFICAR_PRODUCTO;
+		} else {
+			evento = Evento.VISTA_FALLO_MODIFICAR_PRODUCTO;
+		}
+		return new Contexto(evento, exito);
 	}
+
 }
