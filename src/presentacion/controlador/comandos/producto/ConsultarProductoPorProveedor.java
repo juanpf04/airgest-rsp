81103492777c79355dce797ecbda202af0d15c99
acatocaton <alejarel@ucm.es>
2024-11-22 18:39:28 +0100

    Comando ConsultarProductoPorProveedor

--- src/presentacion/controlador/comandos/producto/ConsultarProductoPorProveedor.java
+++ src/presentacion/controlador/comandos/producto/ConsultarProductoPorProveedor.java
@@ -1,27 +1,22 @@
-/**
- * 
- */
 package presentacion.controlador.comandos.producto;
 
 import presentacion.controlador.comandos.Comando;
-import presentacion.controlador.Contexto;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author javia
-* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-*/
+import java.util.List;
+
+import negocio.factoria.FactoriaNegocioMall;
+import negocio.producto.SAProducto;
+import negocio.producto.TProducto;
+import presentacion.controlador.Contexto;
+import presentacion.controlador.Evento;
+
 public class ConsultarProductoPorProveedor implements Comando {
-	/** 
-	* (non-Javadoc)
-	* @see Comando#ejecutar(Object datos)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	@Override
 	public Contexto ejecutar(Object datos) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		FactoriaNegocioMall fn = FactoriaNegocioMall.getInstance();
+		SAProducto sp = fn.crearSAProducto();
+		List<TProducto> producto = sp.consultarProductosPorProveedor((int) datos);
+		Evento evento = Evento.VISTA_RESULTADO_CONSULTAR_PRODUCTOS_POR_PROVEEDOR;
+		return new Contexto(evento, producto);
 	}
 }
