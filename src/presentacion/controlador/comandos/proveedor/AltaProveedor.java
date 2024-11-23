7e6a7623061b30ccd000b05ccd9f4d92527512dc
acatocaton <alejarel@ucm.es>
2024-11-23 19:05:50 +0100

    Comando AltaProveedor

--- src/presentacion/controlador/comandos/proveedor/AltaProveedor.java
+++ src/presentacion/controlador/comandos/proveedor/AltaProveedor.java
@@ -1,27 +1,25 @@
-/**
- * 
- */
 package presentacion.controlador.comandos.proveedor;
 
 import presentacion.controlador.comandos.Comando;
+import negocio.factoria.FactoriaNegocioMall;
+import negocio.proveedor.SAProveedor;
+import negocio.proveedor.TProveedor;
 import presentacion.controlador.Contexto;
+import presentacion.controlador.Evento;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author javia
-* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-*/
+
 public class AltaProveedor implements Comando {
-	/** 
-	* (non-Javadoc)
-	* @see Comando#ejecutar(Object datos)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	@Override
 	public Contexto ejecutar(Object datos) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		FactoriaNegocioMall fn = FactoriaNegocioMall.getInstance();
+		SAProveedor sp = fn.crearSAProveedor();
+		int id = sp.altaProveedor((TProveedor) datos);
+		Evento evento = null;
+		if (id != -1) {
+			evento = Evento.VISTA_EXITO_ALTA_PROVEEDOR;
+		} else {
+			evento = Evento.VISTA_FALLO_ALTA_PROVEEDOR;
+		}
+		return new Contexto(evento, id);
 	}
 }
