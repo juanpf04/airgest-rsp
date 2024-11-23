b66be1640cdc985df4cba93ad9d497bb0967c0b6
acatocaton <alejarel@ucm.es>
2024-11-23 19:17:52 +0100

    Comando AltaDepartamento

--- src/presentacion/controlador/comandos/departamento/AltaDepartamento.java
+++ src/presentacion/controlador/comandos/departamento/AltaDepartamento.java
@@ -1,27 +1,24 @@
-/**
- * 
- */
 package presentacion.controlador.comandos.departamento;
 
 import presentacion.controlador.comandos.Comando;
+import negocio.factoria.FactoriaNegocioMall;
+import negocio.departamento.SADepartamento;
+import negocio.departamento.TDepartamento;
 import presentacion.controlador.Contexto;
+import presentacion.controlador.Evento;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author javia
-* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-*/
 public class AltaDepartamento implements Comando {
-	/** 
-	* (non-Javadoc)
-	* @see Comando#ejecutar(Object datos)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	@Override
 	public Contexto ejecutar(Object datos) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		FactoriaNegocioMall fn = FactoriaNegocioMall.getInstance();
+		SADepartamento sd = fn.crearSADepartamento();
+		int id = sd.altaDepartamento((TDepartamento) datos);
+		Evento evento = null;
+		if (id != -1)
+			evento = Evento.VISTA_EXITO_ALTA_DEPARTAMENTO;
+		else
+			evento = Evento.VISTA_FALLO_ALTA_DEPARTAMENTO;
+
+		return new Contexto(evento, id);
 	}
 }
