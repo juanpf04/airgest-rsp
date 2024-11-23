0d2408627108dcbafe729d5439b54592db3adfa3
acatocaton <alejarel@ucm.es>
2024-11-23 19:18:30 +0100

    Comando ConsultarDepartamentos

--- src/presentacion/controlador/comandos/departamento/ConsultarDepartamentos.java
+++ src/presentacion/controlador/comandos/departamento/ConsultarDepartamentos.java
@@ -1,27 +1,21 @@
-/**
- * 
- */
 package presentacion.controlador.comandos.departamento;
 
 import presentacion.controlador.comandos.Comando;
-import presentacion.controlador.Contexto;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author javia
-* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-*/
+import java.util.List;
+
+import negocio.factoria.FactoriaNegocioMall;
+import negocio.departamento.SADepartamento;
+import negocio.departamento.TDepartamento;
+import presentacion.controlador.Contexto;
+import presentacion.controlador.Evento;
+
 public class ConsultarDepartamentos implements Comando {
-	/** 
-	* (non-Javadoc)
-	* @see Comando#ejecutar(Object datos)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
 	public Contexto ejecutar(Object datos) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		FactoriaNegocioMall fn = FactoriaNegocioMall.getInstance();
+		SADepartamento sd = fn.crearSADepartamento();
+		List<TDepartamento> departamentos = sd.consultarDepartamentos();
+		Evento evento = Evento.VISTA_RESULTADO_CONSULTAR_DEPARTAMENTOS;
+		return new Contexto(evento, departamentos);
 	}
 }
