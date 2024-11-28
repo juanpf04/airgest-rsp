8af226870a7487c4b44c77d032d1b672ba4387ab
paulal20 <PAULA LOPEZ SOLLA>
2024-11-28 10:39:32 +0100

    test alta empleado

--- src/negocio/empleado/SAEmpleadoImp.java
+++ src/negocio/empleado/SAEmpleadoImp.java
@@ -11,38 +11,54 @@ import integracion.factoria.EMFSingleton;
 import negocio.departamento.Departamento;
 import negocio.venta.Venta;
 
-
 public class SAEmpleadoImp implements SAEmpleado {
-	
+
 	public synchronized int altaEmpleado(TEmpleado empleado) {
+		if (!ValidadorEmpleado.comprobarDatos(empleado))
+			return -1;
+
 		EMFSingleton emf = EMFSingleton.getInstance();
 		EntityManager em = emf.getEMF().createEntityManager();
-		try{
+		try {
 
 			em.getTransaction().begin();
+			
 			Departamento d = em.find(Departamento.class, empleado.getIdDepartamento());
 			em.lock(d, LockModeType.OPTIMISTIC_FORCE_INCREMENT);
-			
-			Empleado e = new Empleado(empleado);
-			e.setDepartamento(d);
-			e.setVentas(new ArrayList<Venta>());
-			
-			em.persist(e);
-			
-			em.getTransaction().commit();
-			return e.getId();
-//			Empleado e2 = em.find(Empleado.class,e);
-//			return e2.getId();
-		}catch(Exception e){
+
+			Empleado e = em.createNamedQuery("Empleado.findBytag", Empleado.class).getSingleResult();
+
+			if (e == null) {
+				e = new Empleado(empleado);
+				e.setDepartamento(d);
+				e.setVentas(new ArrayList<Venta>());
+
+				em.persist(e);
+
+				em.getTransaction().commit();
+				return e.getId();
+
+			} else {
+				if (e.getActivo()) {
+					em.getTransaction().rollback();
+					return -1;
+				} else {
+					e.setActivo(true);
+					em.persist(e);
+					em.getTransaction().commit();
+					return e.getId();
+				}
+			}
+
+		} catch (Exception e) {
 			em.getTransaction().rollback();
 			return -1;
-		}finally{
+		} finally {
 			em.close();
 			emf.getEMF().close();
 		}
 	}
 
-	
 	public boolean bajaEmpleado(int id) {
 		// begin-user-code
 		// TODO Auto-generated method stub
@@ -50,7 +66,6 @@ public class SAEmpleadoImp implements SAEmpleado {
 		// end-user-code
 	}
 
-	
 	public TEmpleado consultarEmpleadoPorId(int id) {
 		// begin-user-code
 		// TODO Auto-generated method stub
@@ -58,7 +73,6 @@ public class SAEmpleadoImp implements SAEmpleado {
 		// end-user-code
 	}
 
-	
 	public List<TEmpleado> consultarEmpleados() {
 		// begin-user-code
 		// TODO Auto-generated method stub
@@ -66,7 +80,6 @@ public class SAEmpleadoImp implements SAEmpleado {
 		// end-user-code
 	}
 
-	
 	public boolean modificarEmpleado(TEmpleado empleado) {
 		// begin-user-code
 		// TODO Auto-generated method stub
@@ -74,7 +87,6 @@ public class SAEmpleadoImp implements SAEmpleado {
 		// end-user-code
 	}
 
-	
 	public List<TEmpleado> consultarEmpleadosPorDepartamento(int idDepartamento) {
 		// begin-user-code
 		// TODO Auto-generated method stub
--- tests/negocio/empleado/SAEmpleadoImpTest.java
+++ tests/negocio/empleado/SAEmpleadoImpTest.java
@@ -14,21 +14,19 @@ import negocio.factoria.FactoriaNegocioMallImp;
 public class SAEmpleadoImpTest {
 
 	@Test
-	public int altaEmpleado(TEmpleado empleado) {
+	public void altaEmpleadoTest() {
 		EMFSingleton emf = EMFSingleton.getInstance();
 		EntityManager em = emf.getEMF().createEntityManager();
-
+		em.getTransaction().begin();
 		Departamento d = new Departamento(new TDepartamento(0, "Recursos Humanos", 1, 23.5, true));
 		
 		em.persist(d);
-		
+		em.getTransaction().commit();
 		FactoriaNegocioMall fm = new FactoriaNegocioMallImp();
 		SAEmpleado sa = fm.crearSAEmpleado();
 		
 		int id = sa.altaEmpleado(new TGerente(0, 1, 160, 1, true, 1, 10));
 		
 		assertTrue(id > 0);
-		return 0;
-		
 	}
 }
