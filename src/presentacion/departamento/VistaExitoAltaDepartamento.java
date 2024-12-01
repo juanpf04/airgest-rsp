25d3a891aa615787a1dce4887256539a41bbae7b
dcastr06 <dcastr06@ucm.es>
2024-12-01 13:37:05 +0100

    Algunas vistas de departamento realizadas

--- src/presentacion/departamento/VistaExitoAltaDepartamento.java
+++ src/presentacion/departamento/VistaExitoAltaDepartamento.java
@@ -3,8 +3,17 @@
  */
 package presentacion.departamento;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 import presentacion.Observador;
+import presentacion.UtilidadesP;
 
 /** 
 * <!-- begin-UML-doc -->
@@ -13,15 +22,27 @@ import presentacion.Observador;
 * @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
 */
 public class VistaExitoAltaDepartamento extends JFrame implements Observador {
-	/** 
-	* (non-Javadoc)
-	* @see Observador#actualizar(Object datos)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	public void actualizar(Object datos) {
-		// begin-user-code
-		// TODO Auto-generated method stub
 
-		// end-user-code
+	public void actualizar(Object datos) {
+		UtilidadesP.setAirGestRSP(this);
+		this.setSize(480, 430);
+
+		JPanel principal = new JPanel();
+		principal.setLayout(new BorderLayout());
+
+		JLabel exito = new JLabel("Alta de Departamento exitosa! con id = " + (Integer) datos);
+		exito.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		exito.setHorizontalAlignment(SwingConstants.CENTER);
+		principal.add(exito, BorderLayout.PAGE_START);
+
+		JLabel imagen = new JLabel();
+		imagen.setIcon(new ImageIcon("recursos/iconos/exito.png"));
+		principal.add(imagen, BorderLayout.CENTER);
+
+		this.setContentPane(principal);
+		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		this.setVisible(true);
+		this.setLocation(200, 200);
+		this.setResizable(false);
 	}
 }
--- src/presentacion/departamento/VistaFalloAltaDepartamento.java
+++ src/presentacion/departamento/VistaFalloAltaDepartamento.java
@@ -3,8 +3,17 @@
  */
 package presentacion.departamento;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 import presentacion.Observador;
+import presentacion.UtilidadesP;
 
 /** 
 * <!-- begin-UML-doc -->
@@ -13,15 +22,26 @@ import presentacion.Observador;
 * @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
 */
 public class VistaFalloAltaDepartamento extends JFrame implements Observador {
-	/** 
-	* (non-Javadoc)
-	* @see Observador#actualizar(Object datos)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
 	public void actualizar(Object datos) {
-		// begin-user-code
-		// TODO Auto-generated method stub
+		UtilidadesP.setAirGestRSP(this);
+		this.setSize(600, 660);
 
-		// end-user-code
+		JPanel principal = new JPanel();
+		principal.setLayout(new BorderLayout());
+
+		JLabel exito = new JLabel("Alta de Departamento fallida! :(");
+		exito.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		exito.setHorizontalAlignment(SwingConstants.CENTER);
+		principal.add(exito, BorderLayout.PAGE_START);
+
+		JLabel imagen = new JLabel();
+		imagen.setIcon(new ImageIcon("recursos/iconos/fallo.png"));
+		principal.add(imagen, BorderLayout.CENTER);
+
+		this.setContentPane(principal);
+		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		this.setVisible(true);
+		this.setLocation(200, 200);
+		this.setResizable(false);
 	}
 }
