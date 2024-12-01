916b65c6e7ca23f9f9e404ddd05d35c2051c9349
A-Unii <adrikarp@ucm.es>
2024-12-01 18:34:24 +0100

    Vista Mostrar Proveedores Por Producto

--- src/presentacion/proveedor/VistaMostrarProveedoresPorProducto.java
+++ src/presentacion/proveedor/VistaMostrarProveedoresPorProducto.java
@@ -3,49 +3,81 @@
  */
 package presentacion.proveedor;
 
-import javax.swing.JFrame;
-import presentacion.Observador;
-import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
 
-import negocio.proveedor.TProveedor;
+import presentacion.Observador;
 import presentacion.UtilidadesP;
+import presentacion.controlador.Contexto;
+import presentacion.controlador.Controlador;
+import presentacion.controlador.Evento;
 
 
 public class VistaMostrarProveedoresPorProducto extends JFrame implements Observador {
 	private static final long serialVersionUID = 1L;
 
-	@Override
 	public void actualizar(Object datos) {
 		UtilidadesP.setAirGestRSP(this);
-		this.setSize(400, 450);
+		this.setSize(470, 160);
 
 		JPanel principal = new JPanel();
-		principal.setLayout(new BorderLayout());
+		principal.setLayout(new BoxLayout(principal, BoxLayout.PAGE_AXIS));
 
-		@SuppressWarnings("unchecked")
-		List<TProveedor> proveedores = (List<TProveedor>) datos;
+		JPanel panel_titulo = new JPanel();
+		JLabel titulo = new JLabel("Consultar Proveedores por Producto");
+		titulo.setFont(new Font("Tahoma", Font.BOLD, 30));
+		titulo.setBorder(new LineBorder(Color.BLACK, 2));
+		titulo.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_titulo.add(titulo);
 
-		String s = "";
-		for (TProveedor prov : proveedores)
-			s += prov.toString() + "\n";
+		principal.add(panel_titulo);
+		JPanel centro = new JPanel();
+		centro.setLayout(new BoxLayout(centro, BoxLayout.PAGE_AXIS));
 
-		JTextArea exito = new JTextArea(s);
-		exito.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		principal.add(exito, BorderLayout.PAGE_START);
-		exito.setEditable(false);
+		JPanel id = new JPanel();
+		id.setLayout(new BoxLayout(id, BoxLayout.LINE_AXIS));
+		JLabel etiquetaId = new JLabel("id producto: ");
+		etiquetaId.setFont(new Font("Tahoma", Font.BOLD, 25));
+		JTextField textoId = new JTextField();
+		textoId.setMaximumSize(new Dimension(200, 30));
+		textoId.setMinimumSize(new Dimension(200, 30));
+		textoId.setPreferredSize(new Dimension(200, 30));
+		textoId.setFont(new Font("Tahoma", Font.BOLD, 18));
+		id.add(etiquetaId);
+		id.add(textoId);
+		centro.add(id);
+		principal.add(centro);
 
-		JScrollPane scroll = new JScrollPane(exito, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		principal.add(scroll, BorderLayout.CENTER);
+		Controlador controlador = Controlador.getInstance();
+		JPanel botones = new JPanel();
+
+		JButton aceptar = new JButton("ACEPTAR");
+		aceptar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					int id = Integer.parseInt(textoId.getText());
+					controlador.accion(new Contexto(Evento.CONSULTAR_PROVEEDORES_POR_PRODUCTO, id));
+				} catch (NumberFormatException n) {
+					controlador.accion(new Contexto(Evento.CONSULTAR_PROVEEDORES_POR_PRODUCTO, 0));
+				}
+			}
+		});
+		aceptar.setMaximumSize(new Dimension(100, 30));
+		aceptar.setPreferredSize(new Dimension(100, 30));
 
 		JButton atras = new JButton("ATRAS"); // boton para volver a la ventana
 												// principal
@@ -55,14 +87,21 @@ public class VistaMostrarProveedoresPorProducto extends JFrame implements Observ
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				dispose();
+				controlador.accion(new Contexto(Evento.VISTA_PROVEEDOR, null));
 			}
 		});
 
-		principal.add(atras, BorderLayout.PAGE_END);
+		atras.setMaximumSize(new Dimension(90, 30));
+		atras.setPreferredSize(new Dimension(90, 30));
+
+		botones.add(atras);
+		botones.add(aceptar);
+		principal.add(botones);
 
 		this.setContentPane(principal);
-		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.setLocation(200, 200);
+		this.setResizable(false);
 	}
 }
