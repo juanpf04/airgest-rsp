462f9d0842ecc6b2ca782391b7a329e33aafab4c
acatocaton <alejarel@ucm.es>
2024-12-02 13:57:28 +0100

    vista consultarMarcaPorId

--- src/presentacion/marca/VistaResultadoConsultarMarcaPorId.java
+++ src/presentacion/marca/VistaResultadoConsultarMarcaPorId.java
@@ -1,27 +1,39 @@
-/**
- * 
- */
 package presentacion.marca;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import negocio.marca.TMarca;
 import presentacion.Observador;
+import presentacion.UtilidadesP;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author javia
-* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-*/
 public class VistaResultadoConsultarMarcaPorId extends JFrame implements Observador {
-	/** 
-	* (non-Javadoc)
-	* @see Observador#actualizar(Object datos)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	public void actualizar(Object datos) {
-		// begin-user-code
-		// TODO Auto-generated method stub
 
-		// end-user-code
-	}
+    private static final long serialVersionUID = 1L;
+
+    public void actualizar(Object datos) {
+        UtilidadesP.setAirGestRSP(this);
+        this.setSize(1000, 750);
+
+        JPanel principal = new JPanel();
+        principal.setLayout(new BorderLayout());
+
+        String marca = datos == null ? "" : ((TMarca) datos).toString();
+
+        JTextArea exito = new JTextArea(marca);
+        exito.setEditable(false);
+        exito.setFont(new Font("Tahoma", Font.PLAIN, 26));
+        exito.setAlignmentX(CENTER_ALIGNMENT);
+        principal.add(exito, BorderLayout.PAGE_START);
+
+        this.setContentPane(principal);
+        this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        this.setVisible(true);
+        this.setLocation(200, 200);
+        this.pack();
+    }
 }
