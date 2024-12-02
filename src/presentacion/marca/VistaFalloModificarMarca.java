96ee330d43ec8ce224a5d095b0fbdda74ea5dd27
acatocaton <alejarel@ucm.es>
2024-12-02 13:57:00 +0100

    Vista modificar marca con exito y fallo

--- src/presentacion/marca/VistaFalloModificarMarca.java
+++ src/presentacion/marca/VistaFalloModificarMarca.java
@@ -1,27 +1,47 @@
-/**
- * 
- */
 package presentacion.marca;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 import presentacion.Observador;
+import presentacion.UtilidadesP;
+import presentacion.controlador.Contexto;
+import presentacion.controlador.Controlador;
+import presentacion.controlador.Evento;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author javia
-* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-*/
 public class VistaFalloModificarMarca extends JFrame implements Observador {
-	/** 
-	* (non-Javadoc)
-	* @see Observador#actualizar(Object datos)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	public void actualizar(Object datos) {
-		// begin-user-code
-		// TODO Auto-generated method stub
 
-		// end-user-code
-	}
+    private static final long serialVersionUID = 1L;
+
+    public void actualizar(Object datos) {
+        UtilidadesP.setAirGestRSP(this);
+        this.setSize(600, 660);
+
+        JPanel principal = new JPanel();
+        principal.setLayout(new BorderLayout());
+
+        JLabel exito = new JLabel("Modificación de Marca fallida! :(");
+        exito.setFont(new Font("Tahoma", Font.PLAIN, 29));
+        exito.setHorizontalAlignment(SwingConstants.CENTER);
+        principal.add(exito, BorderLayout.PAGE_START);
+
+        JLabel imagen = new JLabel();
+        imagen.setIcon(new ImageIcon("recursos/iconos/fallo.png"));
+        principal.add(imagen, BorderLayout.CENTER);
+
+        Controlador controlador = Controlador.getInstance();
+        controlador.accion(new Contexto(Evento.VISTA_MARCA, null));
+
+        this.setContentPane(principal);
+        this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        this.setVisible(true);
+        this.setLocation(200, 200);
+        this.setResizable(false);
+    }
 }
