1c6bb19abf9f448a8b56428429c33b532fec29c0
dcastr06 <dcastr06@ucm.es>
2024-12-10 17:51:56 +0100

    Flecos finales (pincelada en la vista principal)

--- src/presentacion/VistaPrincipal.java
+++ src/presentacion/VistaPrincipal.java
@@ -7,11 +7,7 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.LockModeType;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -23,19 +19,6 @@ import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
 import javax.swing.border.MatteBorder;
 
-import integracion.factoria.EMFSingleton;
-import negocio.departamento.Departamento;
-import negocio.departamento.TDepartamento;
-import negocio.empleado.Empleado;
-import negocio.empleado.Gerente;
-import negocio.empleado.TGerente;
-import negocio.marca.Marca;
-import negocio.marca.TMarca;
-import negocio.producto.Producto;
-import negocio.producto.TProducto;
-import negocio.proveedor.Nacional;
-import negocio.proveedor.Proveedor;
-import negocio.proveedor.TNacional;
 import presentacion.controlador.Contexto;
 import presentacion.controlador.Controlador;
 import presentacion.controlador.Evento;
@@ -47,7 +30,7 @@ public class VistaPrincipal extends JFrame implements Observador {
 	@Override
 	public void actualizar(Object datos) {
 		UtilidadesP.setAirGestRSP(this);
-		this.setSize(2000, 2200); //hace que la ventana no salga tan chiquitita
+		this.setSize(850, 650); //hace que la ventana no salga tan chiquitita
 		JPanel principal = new JPanel();
 		principal.setLayout(new BorderLayout());
 
@@ -70,9 +53,10 @@ public class VistaPrincipal extends JFrame implements Observador {
 		principal.add(page_start_panel, BorderLayout.PAGE_START);
 
 		JPanel botones = new JPanel();
-		botones.setLayout(new GridLayout(0, 1, 1, 7));
+		botones.setLayout(new GridLayout(4,3));
 
 		Controlador ctrl = Controlador.getInstance();
+		
 		
 		JLabel imagen = new JLabel();
 		imagen.setHorizontalAlignment(SwingConstants.CENTER);
@@ -228,84 +212,7 @@ public class VistaPrincipal extends JFrame implements Observador {
 		empleado.setToolTipText("MODULO EMPLEADO");
 		botones.add(empleado);
 
-		//-------------------------------------------
-		JButton nuclear = new JButton("nuclear");
-		nuclear.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				//TODO añadir uno de cada entidad a la bbdd
-				EMFSingleton emf = EMFSingleton.getInstance();
-				EntityManager em = emf.getEMF().createEntityManager();
-
-				em.getTransaction().begin();
-
-				Marca marca = new Marca(new TMarca(2, "nike", "alemania", true));
-				Marca marca2 = new Marca(new TMarca(2, "adidas", "alemania", true));
-				Producto p = new Producto(new TProducto(1, "cocacola", 5, 2.20, 82872, 1, true));
-				Nacional n = new Nacional(new TNacional(0, "Nacional1", true, "12345"));
-
-				//Marca
-				em.persist(marca);
-				//em.persist(marca2);
-				
-				//Producto
-				p.setMarca(marca);
-				//marca.getProductos().add(p);
-				em.persist(p);
-
-				em.getTransaction().commit();
-				
-				// La marca tiene el producto vinculado????
-				em.getTransaction().begin();
-				Marca leida = em.find(Marca.class, 1);
-				
-				for (Producto prod : leida.getProductos()){
-					System.out.println(prod.getNombre());
-				}
-				
-				em.getTransaction().commit();
-				
-				//em.persist(marca2);
-
-				/*
-				//Producto
-				List<Proveedor> proveedores = new ArrayList<>();
-				proveedores.add(n);
-				p.setProveedores(proveedores); // Vincular proveedor y producto
-				p.setMarca(marca2);
-				em.persist(p);
-
-				//Proveedor nacional
-				List<Producto> productos = new ArrayList<>();
-				productos.add(p);
-				n.setProductos(productos); // Vincular proveedor y producto
-				em.persist(n);
-
-				//Departamento y Empleado
-				Departamento d = new Departamento(new TDepartamento(0, "Recursos Humanos", 1, 23.5, true));
-
-				Gerente g = new Gerente(new TGerente(0, 1, 160, 1, true, 1, 10));
-				g.setDepartamento(d);
-
-				// Añadir el empleado a la lista que tiene departamento
-				List<Empleado> empleados = new ArrayList<>();
-				empleados.add(g);
-				d.setEmpleados(empleados);
-
-				em.persist(d);
-				em.persist(g);*/
-
-				
-				
-
-				em.close();
-				//emf.getEMF().close();
-			}
-		});
-		nuclear.setToolTipText("nuclear");
-		botones.add(nuclear);
-		//-------------------------------------------
+			//-------------------------------------------
 
 		principal.add(botones, BorderLayout.CENTER);
 
